30a40706d240ea2040a31cfc1ea6cceac2f7cba8
felipe <130618161+fa9958189@users.noreply.github.com>
2023-12-22 11:26:22 -0300

    ew
    trew

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function scheduleAppointment() {
+    const nameInput = document.getElementById('name');
     const dateInput = document.getElementById('date');
     const timeInput = document.getElementById('time');
 
+    const name = nameInput.value;
     const date = dateInput.value;
     const time = timeInput.value;
 
     const appointment = {
+        name,
         date,
         time
     };
 
     saveAppointment(appointment);
-    
-    // Direcionar para a página de agendamentos após o agendamento
-    window.location.href = 'appointments.html';
+    loadAppointments();
+
+    // Limpar os campos após o agendamento
+    nameInput.value = '';
+    dateInput.value = '';
+    timeInput.value = '';
 }
 
 function saveAppointment(appointment) {
@@ -33,11 +39,11 @@ function loadAppointments() {
 
     appointments.forEach((appointment, index) => {
         const listItem = document.createElement('li');
-        listItem.innerHTML = `${appointment.date} às ${appointment.time} <button onclick="deleteAppointment(${index})">Remover</button>`;
+        listItem.innerHTML = `${appointment.name} - ${appointment.date} às ${appointment.time} <button onclick="deleteAppointment(${index})">Remover</button>`;
         appointmentList.appendChild(listItem);
 
         // Adicionar lembrete (aqui é apenas um exemplo, você pode usar uma biblioteca de lembretes mais avançada)
-        setReminder(appointment.date, appointment.time, `Lembrete para o agendamento às ${appointment.time}`);
+        setReminder(appointment.date, appointment.time, `Lembrete para o agendamento de ${appointment.name} às ${appointment.time}`);
     });
 }
 
@@ -52,3 +58,19 @@ function setReminder(date, time, message) {
     // Implemente a lógica de lembrete aqui (pode usar notificações, por exemplo)
     console.log(`Lembrete: ${message} em ${date} às ${time}`);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.getElementById('navbar');
+  
+    // Adiciona um ícone para a navegação em telas pequenas
+    var icon = document.createElement('a');
+    icon.href = 'javascript:void(0);';
+    icon.className = 'icon';
+    icon.innerHTML = '&#9776;'; // &#9776; é o código HTML para o ícone de três linhas (hamburguer)
+    navbar.appendChild(icon);
+  
+    // Adiciona a funcionalidade de exibir/ocultar links ao clicar no ícone
+    icon.addEventListener('click', function() {
+      navbar.classList.toggle('responsive');
+    });
+  });
